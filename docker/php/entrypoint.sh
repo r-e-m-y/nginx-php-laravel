ae401ec20987c792ddf8085fe3cc13a16e93d9e3
Remy Brokke <remy@nlvoorelkaar.nl>
2025-01-19 23:57:18 +0100

    shizzle

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -10,6 +10,14 @@ if [ ! -f "/var/www/database/database.sqlite" ]; then
     touch /var/www/database/database.sqlite
 fi
 
+echo "Run composer install"
+composer install
+echo "Finished composer install"
+
+echo "Build assets"
+npm run build
+echo "Finished building assets"
+
 echo "Init script done. Starting PHP-FPM..."
 
 # Hand off to the CMD
