76daf6c1c2de21277b172d4e7f4da8e597e55e2a
Remy <remybrokke@gmail.com>
2025-10-25 17:38:12 +0200

    Update to Laravel 12 & fix permission isues with WSL

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -3,21 +3,31 @@ set -e
 
 echo "Running container init script..."
 
-php /var/www/artisan key:generate
-
 if [ ! -f "/var/www/database/database.sqlite" ]; then
     echo "Creating SQLite database..."
     touch /var/www/database/database.sqlite
 fi
 
+chown www-data:www-data /var/www/database/database.sqlite
+
 echo "Run composer install"
 composer install
 echo "Finished composer install"
+
+if [ ! -f "/var/www/.env" ]; then
+    echo "Creating .env file from .env.example"
+    cp /var/www/.env.example /var/www/.env
+fi
+
+php /var/www/artisan key:generate
 
 echo "Build assets"
 npm install
 npm run build
 echo "Finished building assets"
+
+echo "Fixing storage permissions for www-data..."
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 
 echo "Init script done. Starting PHP-FPM..."
 
